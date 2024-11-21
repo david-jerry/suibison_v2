daa3ba0e6aa40188f85a39be0b6e81adc055f17e
Jeremiah David <jeremiahedavid@gmail.com>
2024-11-21 19:31:33 +0100

    created a sui wallet app

--- migrations/env.py
+++ migrations/env.py
@@ -48,6 +48,7 @@ def run_migrations_offline() -> None:
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
+        render_as_batch=True,
         dialect_opts={"paramstyle": "named"},
     )
 
@@ -56,7 +57,7 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(connection=connection, target_metadata=target_metadata, render_as_batch=True)
 
     with context.begin_transaction():
         context.run_migrations()
