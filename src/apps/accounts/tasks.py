c5346416afc8d80ba232514d7eb583bc9bb7ec40
Confidence Yobo <confiyobo@gmail.com>
2024-11-27 00:46:30 +0100

    change async handler

--- src/apps/accounts/tasks.py
+++ src/apps/accounts/tasks.py
@@ -36,9 +36,9 @@ def fetch_sui_usd_price_hourly():
 
 @celery_app.task(name="check_and_update_balances")
 def check_and_update_balances():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(fetch_sui_balance())
+    loop = asyncio.get_event_loop()
+    executor = ThreadPoolExecutor()
+    loop.run_in_executor(executor, asyncio.run(fetch_sui_balance()))
     # loop.close()
 
 @celery_app.task(name="run_calculate_daily_tasks")
